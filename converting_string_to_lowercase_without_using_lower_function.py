493966e68648a7932d178ab3298e275bb35fb8b2
KeishaStarberry <jeharakeisha@gmail.com>
2025-04-02 11:55:48 +0800

    Checking and converting the given input using conditions

--- converting_string_to_lowercase_without_using_lower_function.py
+++ converting_string_to_lowercase_without_using_lower_function.py
@@ -1,1 +1,8 @@
 name = input("Enter your name: ")
+lower_casing = ""
+
+if char in name:
+    if "A" <= char <= "Z":
+        lower_casing += chr(ord(char) + 32)
+    else:
+        lower_casing += char
