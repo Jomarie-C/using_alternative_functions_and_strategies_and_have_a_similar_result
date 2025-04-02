3d10df189ca95f4f5575e90699806e9a4208e507
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 00:44:03 +0800

    Alternative startswith() function

--- Batch_7/alternative_startswith_function.py
+++ Batch_7/alternative_startswith_function.py
@@ -1,2 +1,7 @@
 input_string = input("Enter a string: ")
 prefix = input("Enter the prefix to check: ")
+
+if input_string[:len(prefix)] == prefix:
+    print("The string starts with the given prefix.")
+else:
+    print("The string does not start with the given prefix.")
