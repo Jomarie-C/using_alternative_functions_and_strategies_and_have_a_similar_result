1f9c9ebaf687485fa3a2b679890666d6f4800a14
KeishaStarberry <jeharakeisha@gmail.com>
2025-04-02 11:12:21 +0800

    Pseudocode

---
+++ removing_prefix_without_using_removeprefix_function.py
@@ -0,0 +1,9 @@
+# Pseudocode
+    # Ask the user for an input
+    # Ask the user to enter the prefix to be removed
+    # Create a condition that will check the given input if it has a prefix and slice it off
+        # If the input starts with prefix
+            # Remove prefix
+            # Display output
+        # If the input doesn't have prefix
+            # Display output
