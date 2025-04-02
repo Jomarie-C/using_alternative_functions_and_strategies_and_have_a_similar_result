504260f1d7fea3629853925fa32df054a460653c
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 23:19:48 +0800

    Removing ending spaces without using rstring() function

--- Batch_7/removing_ending_spaces_without_using_rstring_function.py
+++ Batch_7/removing_ending_spaces_without_using_rstring_function.py
@@ -1,4 +1,6 @@
 full_name = input("Enter your full name: ")
 
-while input_string.endswith(' '):
-    input_string = input_string[:-1]
+while full_name.endswith(' '):
+    full_name = full_name[:-1]
+
+print("Output:", full_name)
