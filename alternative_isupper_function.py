5a76bc2a31278919f69f7e5cd3349e0418c0f2df
KeishaStarberry <jeharakeisha@gmail.com>
2025-04-02 12:11:37 +0800

    Ask for user input and initializing a boolean variable

--- alternative_isupper_function.py
+++ alternative_isupper_function.py
@@ -1,8 +1,2 @@
-# Pseudocode 
-    # Ask the user for input
-    # Initialize a boolean variable
-    # Create a loop that continuously check the given input if it is in uppercase format
-        # If the input are all in uppercase format
-            # Print 'All characters are in uppercase'
-        # If not
-            # Print 'Not all characters are in uppercase'
+input_string = input("Enter a string: ")
+uppercase = True
