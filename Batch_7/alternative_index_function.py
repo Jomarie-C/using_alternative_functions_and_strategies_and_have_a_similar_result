4511dc01cf27c74332a5364b5dc7cd741c78ed91
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 04:30:01 +0800

    Alternative index function

--- Batch_7/alternative_index_function.py
+++ Batch_7/alternative_index_function.py
@@ -2,3 +2,8 @@ input_string = input("Enter a string: ")
 substring = input("Enter the substring to find: ")
 
 position = input_string.find(substring)
+
+if position != -1:
+    print(f"Found '{substring}' at index {position}.")
+else:
+    print(f"'{substring}' was not found.")
