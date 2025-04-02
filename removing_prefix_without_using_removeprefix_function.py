746d59e07ae3e166f950f5bbbfd2b42c6ef3d9d6
KeishaStarberry <jeharakeisha@gmail.com>
2025-04-02 11:14:11 +0800

    Ask for user input

--- removing_prefix_without_using_removeprefix_function.py
+++ removing_prefix_without_using_removeprefix_function.py
@@ -1,9 +1,2 @@
-# Pseudocode
-    # Ask the user for an input
-    # Ask the user to enter the prefix to be removed
-    # Create a condition that will check the given input if it has a prefix and slice it off
-        # If the input starts with prefix
-            # Remove prefix
-            # Display output
-        # If the input doesn't have prefix
-            # Display output
+input_string = input("Enter a string: ")
+prefix = input("Enter a prefix to remove: ")
