a841a3a5e356a2b712281b1d5d132d5f0099f7ef
KeishaStarberry <jeharakeisha@gmail.com>
2025-04-02 10:35:50 +0800

    Ask the user for an input

--- removing_leading_spaces_without_using_lstrip_function.py
+++ removing_leading_spaces_without_using_lstrip_function.py
@@ -1,9 +1,1 @@
-# Pseudocode
-    # Ask the user for an input
-    # Initialize an empty string
-    # Check the given input and find the non-space character by initializing a boolean variable
-    # Create a loop to check each character until it finds a non-space character
-        # If the conditions were met
-            # Print the correct output
-        # If the given input doesn't have leading spaces
-            # Display the correct output
+full_name = input("Enter your full name: ")
