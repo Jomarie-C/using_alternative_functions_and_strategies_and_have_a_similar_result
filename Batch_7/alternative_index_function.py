c0efb7b5a7f9e40f61257bd77363ce60e64d35a1
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 04:25:28 +0800

    Pseudocode

---
+++ Batch_7/alternative_index_function.py
@@ -0,0 +1,9 @@
+# Pseudocode
+    # Ask the user to enter a string
+    # Ask the user to enter the substring
+    # Initialize the value of the position as the index by using find() function
+    # Create a condition that will display a certain output 
+        # If the substring was found
+            # Print the location of substring
+        # If the substring wasn't found
+            # Print "The substring was not found"
