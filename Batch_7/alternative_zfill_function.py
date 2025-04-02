ec52a5b90eed1768cef4b33df0c28f2e1e4d800f
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 00:59:09 +0800

    Pseudocode

---
+++ Batch_7/alternative_zfill_function.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Ask the user to enter an integer
+    # Ask the user to enter the lenght
+    # Initialize the value of the correct format by using the formatting style we learned in the first semester
+    # Display output without using zfill() function
