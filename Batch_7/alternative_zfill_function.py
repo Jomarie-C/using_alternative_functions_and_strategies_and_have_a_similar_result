b125d3ef0d36dc5b686ece680748460d0f472490
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 01:02:38 +0800

    Ask for user input

--- Batch_7/alternative_zfill_function.py
+++ Batch_7/alternative_zfill_function.py
@@ -1,5 +1,2 @@
-# Pseudocode
-    # Ask the user to enter an integer
-    # Ask the user to enter the lenght
-    # Initialize the value of the correct format by using the formatting style we learned in the first semester
-    # Display output without using zfill() function
+num = int(input("Enter an integer: "))
+lenght = int(input("Enter the lenght: "))
