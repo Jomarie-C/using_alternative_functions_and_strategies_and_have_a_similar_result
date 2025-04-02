5797bc1ae85f123540ac1953eb30e828a4896db2
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 13:56:46 +0800

    Pseudocode

---
+++ alternative_title_function.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Ask the user to enter a string
+    # Initialize a variable with an empty string
+    # Create a loop that continuously checks and converts the given input
+    # Display the expected outcome
