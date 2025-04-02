7c8189c52c4cc643fbbb3b8d92e25edaa4178dbc
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 13:29:22 +0800

    Creating a loop that continuously checks and converts the given input

--- alternative_swapcase_function.py
+++ alternative_swapcase_function.py
@@ -1,2 +1,8 @@
 full_name = input("Enter your name: ")
 reverse_casing = ""
+
+for char in full_name:
+    if char.islower():
+        reverse_casing += char.upper()
+    if char.isupper():
+        reverse_casing += char.lower()
