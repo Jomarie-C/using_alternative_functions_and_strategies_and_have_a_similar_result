4287af895c14db769ea77ce1074a4d0f44dbeab2
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 13:46:13 +0800

    Creating a loop that continuously checks and converts the given input

--- alternative_capitalize_function.py
+++ alternative_capitalize_function.py
@@ -1,2 +1,8 @@
 full_name = input("Enter your full name: ")
 capitalized_name = ""
+
+for char in range(len(full_name)):
+    if char == 0:
+        capitalized_name += full_name[char].isupper()
+    else:
+        capitalized_name += full_name[char].islower()
