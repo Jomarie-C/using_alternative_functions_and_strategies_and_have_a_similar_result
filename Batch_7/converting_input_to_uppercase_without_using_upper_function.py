62c425277ab056e293fb726e0f39a39e6797762f
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 23:48:53 +0800

    Pseudocode

---
+++ Batch_7/converting_input_to_uppercase_without_using_upper_function.py
@@ -0,0 +1,9 @@
+# Pseudocode
+    # Ask the user to enter their full name
+    # Initialize a variable with an empty string
+    # Create a loop that continuously checks and converts each character in the given input
+        # If a character in the given input is in lower case
+            # Convert it to upper case
+            # Print the output with an uppercase format
+        # If a character in the given input is already in upper case
+            # Print the output with an uppercase format
