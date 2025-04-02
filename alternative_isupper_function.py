2ebef226f6e6f6eea64a7cb4b19d0568fdbb4d20
KeishaStarberry <jeharakeisha@gmail.com>
2025-04-02 12:09:47 +0800

    Pseudocode

---
+++ alternative_isupper_function.py
@@ -0,0 +1,8 @@
+# Pseudocode 
+    # Ask the user for input
+    # Initialize a boolean variable
+    # Create a loop that continuously check the given input if it is in uppercase format
+        # If the input are all in uppercase format
+            # Print 'All characters are in uppercase'
+        # If not
+            # Print 'Not all characters are in uppercase'
