8ebdd41a46df1082c2542f8a2802f73ad2360881
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 04:07:19 +0800

    Pseudocode

---
+++ Batch_7/alternative_count_function.py
@@ -0,0 +1,6 @@
+# Pseudocode
+    # Ask the user to enter a string
+    # Ask the user to enter the substring
+    # Initialize the value of count with 0
+    # Create a loop that will check the user input
+    # Display output
