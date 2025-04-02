da2d122df09ee13239a7ac4a7aa354d6b16fcd0a
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 14:01:54 +0800

    Creating a loop that continuously checks and converts the given input

--- alternative_title_function.py
+++ alternative_title_function.py
@@ -1,2 +1,8 @@
 input_string = input("Enter a string: ")
 correct_format = ""
+
+for char in range(len(input_string)):
+    if char == 0 or input_string[char - 1] == " ":
+        correct_format += input_string[char].upper()
+    else:
+        correct_format += input_string[char].lower()
