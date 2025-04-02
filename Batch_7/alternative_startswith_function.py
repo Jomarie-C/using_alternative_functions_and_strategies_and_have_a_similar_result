8f744a167ac2dca6034df7166c4126da14560c51
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 00:40:40 +0800

    Pseudocode

---
+++ Batch_7/alternative_startswith_function.py
@@ -0,0 +1,8 @@
+# Pseudocode
+    # Ask the user to enter a string
+    # Ask the user to enter the prefix to check
+    # Create a condition
+        # If the input starts with the given prefix
+            # Print 'The string starts with the prefix'
+        # If the given prefix doesn't match with the given input
+            # Print 'The string does not start with the given prefix'
