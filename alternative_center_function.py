4fa901c5f36205e49547f7a545ea7b4741d7eb42
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 13:16:32 +0800

    Assigning a value for each variable

--- alternative_center_function.py
+++ alternative_center_function.py
@@ -1,2 +1,7 @@
 input_string = input("Enter a string: ")
 total_lenght = int(input("Enter the total lenght: "))
+
+spaces = total_lenght - len(input_string)
+left_spaces = ' ' * (spaces // 2)
+right_spaces = ' ' * (spaces - len(left_spaces))
+centered_text = left_spaces + text + right_spaces
