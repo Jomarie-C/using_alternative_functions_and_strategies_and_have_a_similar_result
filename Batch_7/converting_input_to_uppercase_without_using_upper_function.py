0094e4f47e352683c278010f5f6b15efcccf99dd
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 00:20:41 +0800

    Creating a loop to check and convert each variable to upper case

--- Batch_7/converting_input_to_uppercase_without_using_upper_function.py
+++ Batch_7/converting_input_to_uppercase_without_using_upper_function.py
@@ -1,2 +1,8 @@
 full_name = input("Enter your full name: ")
 correct_format = ""
+
+for char in input_string:
+    if 'a' <= char <= 'z':  
+        correct_format += chr(ord(char) - 32) 
+    else:
+        correct_format += char
