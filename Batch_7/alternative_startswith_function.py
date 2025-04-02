386b8b2333d4ccf2b5e29359445b21c12b21454f
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 00:42:24 +0800

    Ask for user input

--- Batch_7/alternative_startswith_function.py
+++ Batch_7/alternative_startswith_function.py
@@ -1,8 +1,2 @@
-# Pseudocode
-    # Ask the user to enter a string
-    # Ask the user to enter the prefix to check
-    # Create a condition
-        # If the input starts with the given prefix
-            # Print 'The string starts with the prefix'
-        # If the given prefix doesn't match with the given input
-            # Print 'The string does not start with the given prefix'
+input_string = input("Enter a string: ")
+prefix = input("Enter the prefix to check: ")
