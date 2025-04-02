18de8b0c0248bd95718e5cfaacd34e2dd4528dd5
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 04:11:30 +0800

    Create a loop for checking the user input

--- Batch_7/alternative_count_function.py
+++ Batch_7/alternative_count_function.py
@@ -1,3 +1,7 @@
 input_string = input("Enter a string: ")
 substring = input("Enter the substring to count: ")
 count = 0
+
+for i in range(len(input_string) - len(substring) + 1):
+    if input_string[i:i + len(substring)] == substring:
+        count += 1
