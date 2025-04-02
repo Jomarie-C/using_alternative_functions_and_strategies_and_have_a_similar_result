350422f4065d92f52edad1135f40a7fdac40f281
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 00:51:05 +0800

    Assigning values to each variable

--- Batch_7/alternative_rjust_function.py
+++ Batch_7/alternative_rjust_function.py
@@ -1,2 +1,6 @@
 input_string = input("Enter a string: ")
 total_length = int(input("Enter the total length: "))
+
+if len(input_string) < total_length:
+    spaces_needed = total_length - len(input_string)
+    input_string = ' ' * spaces_needed + input_string
