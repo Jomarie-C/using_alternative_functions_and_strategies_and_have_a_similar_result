ff02bb8968d6eeacb890c140f00fe7af787e66ee
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 12:45:27 +0800

    Alternative endswith() function

--- alternative_endswith_function.py
+++ alternative_endswith_function.py
@@ -1,2 +1,7 @@
 input_string = input("Enter a string: ")
 suffix = input("Enter the suffix you want to check: ")
+
+if input_string[-len(suffix):] == suffix:
+    print("\nOutput: The string ends with the suffix.")
+else:
+    print("\nOutput: The string does not end with the suffix.")
