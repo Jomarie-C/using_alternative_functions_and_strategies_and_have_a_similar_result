de8c36c1f785e52c063eb8324c91dc865b431dbe
KeishaStarberry <jeharakeisha@gmail.com>
2025-04-02 11:49:07 +0800

    Pseudocode

---
+++ converting_string_to_lowercase_without_using_lower_function.py
@@ -0,0 +1,5 @@
+# Pseudocode 
+    # Ask the user for input
+    # Convert the input to lowercase by using an alternative way
+    # Make a loop that continuously check and convert the given input
+    # Print the output in a lower casing format
