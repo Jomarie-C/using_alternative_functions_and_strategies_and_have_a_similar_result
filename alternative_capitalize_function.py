5dd83c7a54ee1326a6520d9e9da4118588753d55
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 13:40:11 +0800

    Pseudocode

---
+++ alternative_capitalize_function.py
@@ -0,0 +1,5 @@
+# Pseudocode
+    # Ask the user to enter their name
+    # Initialize a variable with an empty string
+    # Create a loop that continuously checks and converts the given input
+    # Print the expected output
