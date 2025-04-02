56b84b91cabdd121400e1f70e9ed27855733c82e
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 23:50:40 +0800

    Ask for the user input and initializing a variable with an empty string

--- Batch_7/converting_input_to_uppercase_without_using_upper_function.py
+++ Batch_7/converting_input_to_uppercase_without_using_upper_function.py
@@ -1,9 +1,2 @@
-# Pseudocode
-    # Ask the user to enter their full name
-    # Initialize a variable with an empty string
-    # Create a loop that continuously checks and converts each character in the given input
-        # If a character in the given input is in lower case
-            # Convert it to upper case
-            # Print the output with an uppercase format
-        # If a character in the given input is already in upper case
-            # Print the output with an uppercase format
+full_name = input("Enter your full name: ")
+correct_format = ""
