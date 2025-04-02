99327d8d5d609da0f1b36a5e540cc77d05b6e05b
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 04:38:16 +0800

    Ask for user input

--- Batch_7/alternative_rindex_function.py
+++ Batch_7/alternative_rindex_function.py
@@ -1,9 +1,2 @@
-# Pseudocode
-    # Ask the user to enter a string
-    # Ask the user to enter a substring
-    # Initialize the value of the variable by using rfind() function
-    # Create a condition
-        # If the substring was found
-            # Print the substring together with its location
-        # If the substring wasn't found
-            # Print "The substring was not found"
+input_string = input("Enter a string: ")
+substring = input("Enter the substring to find: ")
