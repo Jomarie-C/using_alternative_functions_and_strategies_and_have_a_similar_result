26129aba3cd2668137d4fbff4f87f3b61c8c7e53
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 13:26:15 +0800

    Ask for the user input and initialize the variable with an empty string

--- alternative_swapcase_function.py
+++ alternative_swapcase_function.py
@@ -1,5 +1,2 @@
-# Pseudocode 
-    # Ask the user to enter their name
-    # Initialize a variable with an empty string
-    # Create a loop that continuously check and convert the given input
-    # Print the output in a reverse casing format
+full_name = input("Enter your name: ")
+reverse_casing = ""
