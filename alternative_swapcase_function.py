3e95f208bfef5f7b9145d8c3c5115d9f242ce560
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 13:23:54 +0800

    Pseudocode

---
+++ alternative_swapcase_function.py
@@ -0,0 +1,5 @@
+# Pseudocode 
+    # Ask the user to enter their name
+    # Initialize a variable with an empty string
+    # Create a loop that continuously check and convert the given input
+    # Print the output in a reverse casing format
