a986913ab3b143029b9882c65331e9a761295fc0
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 00:33:32 +0800

    Initializing a boolean variable and creating a loop that checks if all of the characters in the given input are all in lower case

--- Batch_7/alternative_islower_function.py
+++ Batch_7/alternative_islower_function.py
@@ -1,1 +1,9 @@
 input_string = input("Enter a string: ")
+correct_format = True
+
+for char in input_string:
+    if 'a' <= char <= 'z':
+        continue
+    elif char.isalpha():
+        correct_format = False
+        break
