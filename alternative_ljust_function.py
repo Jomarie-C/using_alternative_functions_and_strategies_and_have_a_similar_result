11d5140b1aa1a857160f6354daeba2cd2c5b09d3
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 12:56:30 +0800

    Alternative ljust() function

--- alternative_ljust_function.py
+++ alternative_ljust_function.py
@@ -2,3 +2,4 @@ input_string = input("Enter a string: ")
 total_lenght = int(input("Enter the lenght: "))
 
 alternative_ljust = input_string + ' ' * (total_lenght - len(input_string))
+print("\nOutput:", alternative_ljust)
