b11c56e1cf38c6d63eee5ee9e7eb048f4970bd5c
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-02 13:19:23 +0800

    Alternative center function

--- alternative_center_function.py
+++ alternative_center_function.py
@@ -4,4 +4,6 @@ total_lenght = int(input("Enter the total lenght: "))
 spaces = total_lenght - len(input_string)
 left_spaces = ' ' * (spaces // 2)
 right_spaces = ' ' * (spaces - len(left_spaces))
-centered_text = left_spaces + text + right_spaces
+centered_text = left_spaces + input_string + right_spaces
+
+print("\nOutput:", centered_text)
