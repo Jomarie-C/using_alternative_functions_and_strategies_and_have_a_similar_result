4b517080f2bfe0c9bb98d77bb0d3b89e0272c408
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 00:22:48 +0800

    Converting input to uppercase without using upper() function

--- Batch_7/converting_input_to_uppercase_without_using_upper_function.py
+++ Batch_7/converting_input_to_uppercase_without_using_upper_function.py
@@ -1,8 +1,10 @@
 full_name = input("Enter your full name: ")
 correct_format = ""
 
-for char in input_string:
+for char in full_name:
     if 'a' <= char <= 'z':  
         correct_format += chr(ord(char) - 32) 
     else:
         correct_format += char
+
+print("Output:", correct_format)
