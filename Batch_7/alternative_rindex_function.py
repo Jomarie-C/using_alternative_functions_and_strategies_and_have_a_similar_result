22a19531aeb3485c42ad150589f023c1d884818c
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-04-03 04:40:54 +0800

    Alternative rindex() function

--- Batch_7/alternative_rindex_function.py
+++ Batch_7/alternative_rindex_function.py
@@ -1,3 +1,8 @@
 input_string = input("Enter a string: ")
 substring = input("Enter the substring to find: ")
 position = input_string.rfind(substring)
+
+if position != -1:
+    print(f"Found '{substring}' at index {position}.")
+else:
+    print(f"'{substring}' was not found.")
