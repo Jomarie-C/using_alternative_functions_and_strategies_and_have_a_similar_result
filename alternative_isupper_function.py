5cf677385627c3d3163cf4fb0a1683816eb7a4d2
KeishaStarberry <jeharakeisha@gmail.com>
2025-04-02 12:17:11 +0800

    Alternative isupper() function

--- alternative_isupper_function.py
+++ alternative_isupper_function.py
@@ -5,3 +5,8 @@ for char in input_string:
     if 'a' <= char <= 'z':
         uppercase = False
         break
+
+if uppercase:
+    print("All characters are in uppercase.")
+else:
+    print("Not all characters are in uppercase.")
