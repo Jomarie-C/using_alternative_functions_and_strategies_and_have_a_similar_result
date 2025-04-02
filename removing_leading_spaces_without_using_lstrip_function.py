e645ef8ca6ad1f9511d6ec137f190bed5693d11c
KeishaStarberry <jeharakeisha@gmail.com>
2025-04-02 10:48:58 +0800

    Creating a loop to check a non-space character

--- removing_leading_spaces_without_using_lstrip_function.py
+++ removing_leading_spaces_without_using_lstrip_function.py
@@ -1,3 +1,11 @@
 full_name = input("Enter your full name: ")
 correct_format = ""
 non_space_character = False
+
+for char in full_name:
+    if not non_space_character:
+        if char != " ":
+            non_space_character = True
+            correct_format += char
+    else:
+        correct_format += char
